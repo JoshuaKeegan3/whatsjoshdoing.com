f9aa72e6d15ebd78c5155470a2e610fbc3c0dc19
Joshua Keegan <keeganjosh@vuw.ac.nz>
2025-09-17 18:34:04 +1200

    initial site with filler text
    TODO: add convex integration

---
+++ components/important_text.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useState, useEffect } from "react";
+export default function ImportantText(props: any) {
+  const [animateText, setAnimateText] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setAnimateText(true);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+  return (
+    <div
+      className={`
+        animated-gradient
+        tracking-[-.01em] pl-1.5 pr-1.5
+        transition-all duration-700 ease-out transform origin-center
+        bg-gradient-to-r from-purple-500 via-pink-500 to-yellow-500 bg-clip-text text-transparent
+        ${props.zoom ? `${animateText ? "opacity-100 scale-100" : "opacity-100 scale-[5]"}` : ""}
+      `}
+    >
+      {props.text}
+    </div>
+  );
+}
