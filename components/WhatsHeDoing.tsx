57db644bbca769bbae7359b2d8f3aa3c64a590c9
Joshua Keegan <keeganjosh@vuw.ac.nz>
2025-09-30 13:50:16 +1300

    Added status text

--- components/WhatsHeDoing.tsx
+++ components/WhatsHeDoing.tsx
@@ -15,44 +15,36 @@ type Activity = {
 
 type Status = "Offline" | "Online";
 
+const INACTIVE_OPTIONS = [
+  "Definitely Vibing",
+  "Probably Rock Hugging",
+  "Dreaming of Code",
+  "Wishing you a good day",
+];
+
 export default function WhatsHeDoing() {
   const activities: any = useQuery(api.activity.get);
   if (activities == undefined) {
     return;
   }
-  let id = activities[0]._id;
   let project_name = activities[0].project_name;
   let file_name = activities[0].file_name;
   let class_name = activities[0].class_name;
   let function_name = activities[0].function_name;
+  // let repo = activities[0].repo;
+  // let readme = activities[0].readme;
 
   let creation_time = activities[0]._creationTime;
-  let now = new Date();
+  let now = new Date().getTime();
 
-  let status: Status = "Online";
-  // if the time difference is greater than 30 minutes
+  // if the time difference is greater than 60 minutes
   // set offline to true
+  let status: Status = 60 * 60 > now - creation_time ? "Online" : "Offline";
 
-  let output_text = (
-    <div className="flex flex-col">
-      <div className="justify-center flex flex-row">
-        {"Currently working on"}
-        <ImportantText zoom={false} text={project_name} />
-      </div>
-
-      <div className="justify-center flex flex-row">
-        {"Modifying"}
-        <ImportantText zoom={false} text={function_name} />
-        {"in"}
-        <ImportantText zoom={false} text={file_name} />
-      </div>
-    </div>
-  );
-
-  let status_block = undefined;
-
+  let status_marker = undefined;
+  let status_text = undefined;
   if (status == "Online") {
-    status_block = (
+    status_marker = (
       <div className="text-3xl flex flex-row p-4 rounded-lg">
         <span className="relative flex size-3">
           <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-emerald-500 opacity-75"></span>{" "}
@@ -60,20 +52,41 @@ export default function WhatsHeDoing() {
         </span>
       </div>
     );
+
+    status_text = (
+      <div className="flex flex-col">
+        <div className="justify-center flex flex-row">
+          {"Currently working on"}
+          <ImportantText zoom={false} text={project_name} />
+        </div>
+
+        <div className="justify-center flex flex-row">
+          {"Modifying"}
+          <ImportantText zoom={false} text={function_name} />
+          {"in"}
+          <ImportantText zoom={false} text={file_name} />
+        </div>
+      </div>
+    );
   } else {
-    status_block = (
+    status_marker = (
       <div className="text-3xl flex flex-row p-4 rounded-lg">
         <span className="relative flex size-3">
           <span className="relative inline-flex size-3 rounded-full bg-red-700"></span>
         </span>
       </div>
     );
+    status_text = (
+      <p className="text-3xl flex flex-row p-4 rounded-lg">
+        {INACTIVE_OPTIONS[Math.floor(INACTIVE_OPTIONS.length * Math.random())]}
+      </p>
+    );
   }
 
   return (
     <div className="fade-in leading-normal text-3xl flex flex-row p-4 rounded-lg">
-      {output_text}
-      {status_block}
+      {status_text}
+      {status_marker}
     </div>
   );
 }
