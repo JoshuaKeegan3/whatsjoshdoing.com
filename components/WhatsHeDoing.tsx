4b9f8155e6529e83ab89fdaff3fd49f05d0c0d70
Joshua Keegan <keeganjosh@vuw.ac.nz>
2025-10-02 14:12:09 +1300

    its gone white?

--- components/WhatsHeDoing.tsx
+++ components/WhatsHeDoing.tsx
@@ -5,6 +5,8 @@ import ImportantText from "@/components/ImportantText";
 import { api } from "../convex/_generated/api";
 import Link from "next/link";
 import z from "zod";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "./ui/hover-card";
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 
 type Activity = {
   _id: string;
@@ -52,6 +54,7 @@ export default function WhatsHeDoing() {
   // if the time difference is greater than 60 minutes
   // set offline to true
   let status: Status = 60 * 60 > now - creation_time ? "Online" : "Offline";
+  status = 60 * 60 < now - creation_time ? "Online" : "Offline";
 
   let status_marker = undefined;
   let status_text = undefined;
@@ -69,7 +72,28 @@ export default function WhatsHeDoing() {
       <div className="flex flex-col">
         <div className="justify-center flex flex-row">
           {"Currently working on"}
-          <ImportantText zoom={false} text={project_name} />
+          <HoverCard>
+            <HoverCardTrigger>
+              <ImportantText zoom={false} text={project_name} />
+            </HoverCardTrigger>
+            <HoverCardContent className="w-80">
+              <div className="flex justify-between gap-4">
+                <Avatar>
+                  <AvatarImage src="https://github.com/vercel.png" />
+                  <AvatarFallback>VC</AvatarFallback>
+                </Avatar>
+                <div className="space-y-1">
+                  <h4 className="text-sm font-semibold">@nextjs</h4>
+                  <p className="text-sm">
+                    The React Framework – created and maintained by @vercel.
+                  </p>
+                  <div className="text-muted-foreground text-xs">
+                    Joined December 2021
+                  </div>{" "}
+                </div>
+              </div>
+            </HoverCardContent>
+          </HoverCard>
         </div>
 
         <div className="justify-center flex flex-row">
