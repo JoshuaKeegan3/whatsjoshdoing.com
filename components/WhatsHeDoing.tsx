0d15173155523972e2b32ec70763ba4fd8377fb7
Joshua Keegan <keeganjosh@vuw.ac.nz>
2025-10-02 15:08:01 +1300

    Themeing changes and Re-added inactive ping

--- components/WhatsHeDoing.tsx
+++ components/WhatsHeDoing.tsx
@@ -108,6 +108,7 @@ export default function WhatsHeDoing() {
     status_marker = (
       <div className="text-3xl flex flex-row p-4 rounded-lg">
         <span className="relative flex size-3">
+          <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-700 opacity-75"></span>
           <span className="relative inline-flex size-3 rounded-full bg-red-700"></span>
         </span>
       </div>
